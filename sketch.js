83dbfada215f367120a799c4dce8d1f2829b791e
juhiagrawal1 <juhiagrawal1@gmail.com>
2020-11-09 11:37:20 +0530

    Platform added

--- sketch.js
+++ sketch.js
@@ -15,8 +15,9 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
+    platform = new Ground(600,200,1200,20);
     
-    ground = new Ground(600,height,1200,20)
+    ground = new Ground(600,height,1200,20);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -59,4 +60,5 @@ function draw(){
     log5.display();
 
     bird.display();
+    platform.display();
 }
